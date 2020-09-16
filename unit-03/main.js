8ce0e74116766aa3eb559a2154fcb0cdcb921e5e
BlakeAllred <blake.allred95@icloud.com>
2020-09-16 13:41:59 -0600

    wok

--- unit-03/main.js
+++ unit-03/main.js
@@ -1,4 +1,4 @@
-
+// animation
 const keyframes = [
      { transform: 'translate(1px, 1px) rotate(0deg)' },
    { transform: 'translate(-1px, -2px) rotate(-1deg)' },
@@ -13,15 +13,79 @@ const keyframes = [
    { transform: 'translate(1px, -2px) rotate(-1deg)' }
   ];
 
-const hide = function() {
-    document.querySelector('img').style.visibility='hidden'
-}
+
+
+
 
 
 
 function handleClick() {
     document.querySelector('img').animate(keyframes, 400)
-    // setTimeout(hide, 301);
+    
     
 }
+
+// animation
+
+
+
+
+const secretNumber = Math.floor((Math.random() * 15) + 1);
+    console.log(secretNumber)
+
+
+
+let attempt = 0
+
+    function plays() {
+    
+    let myNumber = document.querySelector('#guess').value
+    if (myNumber == secretNumber) {
+        let keyz = [
+            { opacity: 1 },
+            { opacity: 0}
+          ];
+        const hide = function() {
+            
+            document.querySelector('img').style.visibility='hidden'
+        }
+        function fade(){
+            document.querySelector('img').animate(keyz, 400)
+            setTimeout(hide, 401)
+        }
+        switch (true) {
+            case (attempt < 3):
+                fade()
+                break;
+            case (attempt < 7):
+                fade()
+                break;
+            default: fade()
+                break;
+        }
+    }
+    else if (myNumber == 0 || myNumber > 15) {
+        document.querySelector('.feedback').innerText = 'feedback: Number is out of range'
+    }
+    else if (myNumber > secretNumber) {
+        attempt += 1
+        document.querySelector('.trys').innerText = `trys: ${attempt}`
+        document.querySelector('.feedback').innerText = 'feedback: too high'
+        document.querySelector('img').animate(keyframes, 400)
+
+        
+    }
+    else if (myNumber < secretNumber) {
+       attempt += 1
+       document.querySelector('.trys').innerText = `trys: ${attempt}`
+       document.querySelector('.feedback').innerText = 'feedback: too low'
+        document.querySelector('img').animate(keyframes, 400)
+        
+    }
+    }
+
+    
+
+
+
 
