56fc2b52eb994c5d2385fb0cb0dd1d803c1e075e
BlakeAllred <blake.allred95@icloud.com>
2020-09-30 20:11:51 -0600

    removed hype

--- unit-01/main.js
+++ unit-01/main.js
@@ -1,14 +1,14 @@
 const userName = prompt('enter name')
 console.log(userName)
 
+var d = Date(Date.now()); 
 
 document.querySelector('#company').innerHTML='Chewys bed and breakfast'
 document.querySelector('#slogan').innerHTML = 'from a galaxy far far away'
-// hypeDocument.querySelector('.HYPE_element').innerHTML=`Hello ${userName}, I am C3P0 Human Cyborg Relations!`
-// document.querySelector('#text').innerText='hello there'
+document.querySelector('.text-box').innerHTML = `Hello ${userName}. I am C3PO Human Cyborg Relations. The time is ${d}`
 
 
-var d = Date(Date.now()); 
+
                 
 
 
