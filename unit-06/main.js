ccfa473a3ea076b1e020888c8af8bd90f2f0bb92
BlakeAllred <blake.allred95@icloud.com>
2020-10-07 21:13:27 -0600

    fixed error checking bug in unit-06

--- unit-06/main.js
+++ unit-06/main.js
@@ -15,7 +15,7 @@ const splitter = () => {
 }
 
 const story = () => {
-    if(nounArray[0].length === 3 && adjArray[0].length === 3 && verbArray[0].length === 3)
+    if(nounArray[0].length >= 3 && adjArray[0].length >= 3 && verbArray[0].length >= 3)
     {
         document.querySelector('.story').innerHTML=(`One day there was a ${nounArray[0][0].toLowerCase()} who liked to ${verbArray[0][0].toLowerCase()}. Now, while this happened, Mr. ${nounArray[0][1].toLowerCase()} was quite ${adjArray[0][0].toLowerCase()}. This is because ${nounArray[0][2].toLowerCase()} was always left out so ${nounArray[0][2].toLowerCase()} decided to ${verbArray[0][1].toLowerCase()} ${nounArray[0][0].toLowerCase()} very ${adjArray[0][1].toLowerCase()}ingly. This made ${nounArray[0][0].toLowerCase()} feel ${adjArray[0][2].toLowerCase()} so he ${verbArray[0][2].toLowerCase()}ed ${nounArray[0][1].toLowerCase()}. The End...`)
 
