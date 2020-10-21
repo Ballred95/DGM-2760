9156aa73ed3bd8e3be1280954ed5efab3b2b743a
BlakeAllred <blake.allred95@icloud.com>
2020-10-20 23:46:10 -0600

    added unit 09

---
+++ unit-09/starter/js/main.js
@@ -0,0 +1,21 @@
+function duplicateMenu() {
+
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
+
+    let newList = document.createElement('ul')
+
+    topList.forEach(menuItem=> {
+        let newLI = document.createElement('li')
+        let newLink = document.createElement('a')
+        newLI.appendChild(newLink)
+        newLink.setAttribute('href', menuItem.getAttribute('href'))
+        newLink.textContent = menuItem.textContent
+        newList.appendChild(newLI)
+        document.querySelector('nav#smallNavArea').appendChild(newList)
+
+        
+    })
+}
+
+
+duplicateMenu()
